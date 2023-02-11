98d3978743590ae743e7315c7c07b6dc5f8de16a
tanpoya <tanpoya02@gmail.com>
2023-02-11 19:06:57 +0900

    1

---
+++ html/index/js/script.js
@@ -0,0 +1,26 @@
+// $(() => {
+
+// });
+
+if (true && false) {
+    console.log("A");
+} else {
+    console.log("B");
+}
+
+let z = true;
+let x = 1;
+let y = 2;
+console.log(z ? x:y);
+
+const result = 10 + "10";
+console.log(result);
+
+let num = 10;
+let strNum = "10";
+if(String(num) === strNum) {
+    console.log(`equals`);
+}
+
+let q1 = 10 === 10 || 10 !== "10";
+console.log(q1);
