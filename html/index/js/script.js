22f6c5789271a62848212a0cc684759f1a4c57ee
tanpoya <tanpoya02@gmail.com>
2023-02-13 17:59:29 +0900

    Update script.js

--- html/index/js/script.js
+++ html/index/js/script.js
@@ -2,25 +2,44 @@
 
 // });
 
-if (true && false) {
-    console.log("A");
-} else {
-    console.log("B");
+// if (true && false) {
+//     console.log("A");
+// } else {
+//     console.log("B");
+// }
+
+// let z = true;
+// let x = 1;
+// let y = 2;
+// console.log(z ? x:y);
+
+// const result = 10 + "10";
+// console.log(result);
+
+// let num = 10;
+// let strNum = "10";
+// if(String(num) === strNum) {
+//     console.log(`equals`);
+// }
+
+// let q1 = 10 === 10 || 10 !== "10";
+// console.log(q1);
+
+let food = "melon";
+switch (food) {
+    case "melon":
+        console.log("멜론");
+        break;
+    case "apple":
+        console.log("사과");
+        break;
+    case "banana":
+        console.log("바나나");
+        break;
+    case "carrot":
+        console.log("carrot");
+        break;
+    default:
+        console.log("default");
+        break;
 }
-
-let z = true;
-let x = 1;
-let y = 2;
-console.log(z ? x:y);
-
-const result = 10 + "10";
-console.log(result);
-
-let num = 10;
-let strNum = "10";
-if(String(num) === strNum) {
-    console.log(`equals`);
-}
-
-let q1 = 10 === 10 || 10 !== "10";
-console.log(q1);
